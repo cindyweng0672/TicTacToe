012405d5194c56cf142eaf0ff824b465607f357d
cindyweng672 <84527361+cindyweng0672@users.noreply.github.com>
2023-11-14 09:33:36 -0800

    Add files via upload

--- TicTacToe.java
+++ TicTacToe.java
@@ -15,6 +15,7 @@ public class TicTacToe
     public static int roundCount=1;
     public static boolean player1winner=false;
     public static boolean player2winner=false; 
+    public static boolean boardFull=false;
 
     public static void TicTacToe()
     {
@@ -23,8 +24,9 @@ public class TicTacToe
 
     public static void takeInput(){
         Scanner sc=new Scanner(System.in);
-
-        if(!player1winner&&!player2winner){
+        
+    
+        if(!player1winner&&!player2winner&&!boardFull){ //make new moves
             System.out.println("Round "+roundCount+" :");
 
             print2DArray(board);
@@ -40,12 +42,13 @@ public class TicTacToe
 
             newMove(inputTemp[0], inputTemp[1]);
 
-        }else{
+        }else{ //has a winner or the board is full
             String input=sc.nextLine();
-            if(input.equals("Y")){
+            if(input.equals("Y")){ //reset everything to restart
                 player1winner=false;
                 player2winner=false;
                 roundCount=1;
+                boardFull=false;
                 player1=true;
                 for(int i=0; i<3; i++){
                     for(int j=0; j<3; j++){
@@ -53,9 +56,9 @@ public class TicTacToe
                     }
                 }
                 takeInput();
-            }else if(input.equals("N")){
+            }else if(input.equals("N")){ //don't restart
                 return;
-            }else{
+            }else{ //invalid input (not Y or N)
                 System.out.println("Invalid Input: please enter Y or N");
                 takeInput();
             }
@@ -63,20 +66,22 @@ public class TicTacToe
 
     }
 
-    public static void newMove(String r, String c){
+    public static void newMove(String r, String c){//make new moves 
+        //turn string into int
         int ri=Integer.valueOf(r);
         int ci=Integer.valueOf(c);
 
+        //check if the coordinate is out of bound
         if(ri>=3 || ci>=3){
             System.out.println("Invalid Input: Try Again(Out of Bound)!");
             takeInput();
             return;
         }else{
-            if(board[ri][ci]!=null){
+            if(board[ri][ci]!=null){ //duplicate input
                 System.out.println("Invalid Input: Try Again(Grid Occupied!)");
                 takeInput();
                 return;
-            }else{
+            }else{ 
                 if(player1){
                     board[ri][ci]="X";
                 }else{
@@ -85,13 +90,15 @@ public class TicTacToe
                 roundCount++;
                 player1=!player1;
                 checkWinner(board);
-                if(!player1winner&&!player2winner){
+                if(!player1winner&&!player2winner&&!boardFull){
                     takeInput();
-                }else{
+                }else{ //if there's a winner or the board is full -> end the game
                     if(player1winner){
                         System.out.println("Player 1 Won!");
                     }else if(player2winner){
                         System.out.println("Player 2 Won!");
+                    }else if(boardFull){
+                        System.out.println("Board Full! It's a tie!");
                     }
                     print2DArray(board);
                     System.out.println("Play again? Y/N");
@@ -115,55 +122,53 @@ public class TicTacToe
     }
 
     public static void checkWinner(String[][] arr2d){
-        for(int r=0; r<arr2d.length; r++){
-            int countplayer1=0;
-            int countplayer2=0;
-            for(int c=0; c<arr2d[0].length; c++){
-                if(arr2d[r][c]==null){
-                    return;
-                }
-                if(arr2d[r][c].equals("X")){
-                    countplayer1++;
-                }else if(arr2d[r][c].equals("Y")){
-                    countplayer2++;
-                }
-            }
-            if(countplayer1==3){
-                player1winner=true;
-                return;
-            }else if(countplayer2==3){
-                player2winner=true;
-                return;
-            }
-        }
-
-        for(int c=0; c<arr2d[0].length; c++){
-            int countplayer1=0;
-            int countplayer2=0;
-            for(int r=0; r<arr2d.length; r++){
-                if(arr2d[r][c]==null){
-                    return;
-                }
-                if(arr2d[r][c].equals("X")){
-                    countplayer1++;
-                }else if(arr2d[r][c].equals("Y")){
-                    countplayer2++;
-                }
-            }
-            if(countplayer1==3){
-                player1winner=true;
-                return;
-            }else if(countplayer2==3){
-                player2winner=true;
-                return;
-            }
-        }
-
         int countplayer1=0;
         int countplayer2=0;
+        
+        //check1: row then column
+        for(int r=0; r<arr2d.length; r++){
+            countplayer1=0;
+            countplayer2=0;
+            for(int c=0; c<arr2d[0].length; c++){
+                if(arr2d[r][c]==null){
+                    break;
+                }
+                if(arr2d[r][c].equals("X")){
+                    countplayer1++;
+                }else if(arr2d[r][c].equals("Y")){
+                    countplayer2++;
+                }
+            }
+            if(has3Points(countplayer1, countplayer2)){
+                return;
+            }
+        }
+        
+        //check2: column then row
+        for(int c=0; c<arr2d[0].length; c++){
+            countplayer1=0;
+            countplayer2=0;
+            for(int r=0; r<arr2d.length; r++){
+                if(arr2d[r][c]==null){
+                    break;
+                }
+                if(arr2d[r][c].equals("X")){
+                    countplayer1++;
+                }else if(arr2d[r][c].equals("Y")){
+                    countplayer2++;
+                }
+            }
+            if(has3Points(countplayer1, countplayer2)){
+                return;
+            }
+        }
+
+        //check3: left to right diagonal
+        countplayer1=0;
+        countplayer2=0;
         for(int r=0; r<arr2d.length; r++){
             if(arr2d[r][r]==null){
-                return;
+                break;
             }
             if(arr2d[r][r].equals("X")){
                 countplayer1++;
@@ -171,33 +176,47 @@ public class TicTacToe
                 countplayer2++;
             }
         }
-        if(countplayer1==3){
-            player1winner=true;
-            return;
-        }else if(countplayer2==3){
-            player2winner=true;
+        if(has3Points(countplayer1, countplayer2)){
             return;
         }
 
+
+        //check4: right to left diagonal
         countplayer1=0;
         countplayer2=0;
         for(int r=0; r<arr2d.length; r++){
-            if(arr2d[r][3-r]==null){
-                return;
+            if(arr2d[r][2-r]==null){
+                break;
             }
-            if(arr2d[r][3-r].equals("X")){
+            if(arr2d[r][2-r].equals("X")){
                 countplayer1++;
-            }else if(arr2d[r][3-r].equals("Y")){
+            }else if(arr2d[r][2-r].equals("Y")){
                 countplayer2++;
             }
         }
-        if(countplayer1==3){
-            player1winner=true;
-            return;
-        }else if(countplayer2==3){
-            player2winner=true;
+        if(has3Points(countplayer1, countplayer2)){
             return;
         }
+        
+        //check if the board is full
+        boardFull=true;
+        for(int r=0; r<3; r++){
+            for(int c=0; c<3; c++){
+                if(board[r][c]==null){
+                    boardFull=false;
+                    return;
+                }
+            }
+        }
+    }
+
+    public static boolean has3Points(int p1w, int p2w){
+        if(p1w==3){
+            player1winner=true;
+        }else if(p2w==3){
+            player2winner=true;
+        }
+        return false;
     }
 
 }
